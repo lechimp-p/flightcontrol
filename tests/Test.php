beede34c806922662ef05637b161081e82fe78a2
Richard Klees <richard.klees@rwth-aachen.de>
2015-07-19 14:22:40 +0200

    removed template for Tests.

--- tests/Test.php
+++ tests/Test.php
@@ -1,6 +1,6 @@
 <?php
 /******************************************************************************
- * DESCRIPTION
+ * An iterator interface over the Leagues flysystem.
  * Copyright (c) 2014, 2015 Richard Klees <richard.klees@rwth-aachen.de>
  *
  * This software is licensed under The MIT License. You should have received 
