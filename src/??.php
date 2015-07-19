9824b808ec86a1c952b76ecaf01e8a6dc67ff6cf
Richard Klees <richard.klees@rwth-aachen.de>
2015-07-19 20:01:06 +0200

    removed template php file

--- src/??.php
+++
@@ -1,9 +0,0 @@
-<?php
-/******************************************************************************
- * DESCRIPTION
- * Copyright (c) 2014 Richard Klees <richard.klees@rwth-aachen.de>
- *
- * This software is licensed under The MIT License. You should have received 
- * a copy of the along with the code.
- */
-
