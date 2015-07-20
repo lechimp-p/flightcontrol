004ccebdf119fd4de2937cc5582528b9090b6cba
Richard Klees <richard.klees@rwth-aachen.de>
2015-07-20 19:25:09 +0200

    added missing PerformDirectoryIterator

---
+++ src/PerformDirectoryIterator.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Lechimp\Flightcontrol;
+
+class PerformDirectoryIterator extends DirectoryIterator {
+    /**
+     * @var \Closure 
+     */
+    protected $action;
+
+    /**
+     * @var DirectoryIterator
+     */
+    protected $prev;
+
+    public function __construct(\Closure $action, DirectoryIterator $prev) {
+        $this->action = $action;
+        $this->prev = $prev;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function onContents(\Closure $action) {
+        $local_action = $this->action;
+        $this->prev->onContents(function(FSObject $obj) use ($action, $local_action) {
+            $local_action($obj);
+            $action($obj);
+        }); 
+    }
+}
