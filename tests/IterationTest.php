1a3884e5c1438e3723150b83bae6c5e9aa4bec7e
Richard Klees <richard.klees@rwth-aachen.de>
2015-07-31 18:43:11 +0200

    moved iteration with filters to general iteration tests.

--- tests/IterationTest.php
+++ tests/IterationTest.php
@@ -49,4 +49,56 @@ class WithContentsTest extends _TestCaseBase {
         $this->assertContains("dir_2_1", $accu);
         $this->assertContains("file_2_1", $accu);
     }
+
+    public function test_filterWorks() {
+        $root = $this->flightcontrol->directory("/root");
+        $accu = array();
+        $root->withContents()
+             ->filter(function(\Lechimp\Flightcontrol\FSObject $obj) {
+                    return $obj->name() == "dir_1";
+                })
+             ->perform(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                    $accu[] = $obj->name();
+                })
+             ->run();
+        $this->assertEquals(array("dir_1"), $accu);
+    } 
+
+    public function test_filterDirectories() {
+        $dir_2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir_2->withContents()
+              ->directoriesOnly()
+              ->perform(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                    $accu[] = $obj->name();
+                })
+              ->run();
+        $this->assertEquals(array("dir_2_1"), $accu);
+    }
+
+    public function test_filterFiles() {
+        $dir_2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir_2->withContents()
+              ->filesOnly()
+              ->perform(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                    $accu[] = $obj->name();
+                })
+              ->run();
+        $this->assertEquals(array("file_2_1"), $accu);
+    }
+
+    public function test_filterNamed() {
+        $dir2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir2->withContents()
+             ->named("dir.*")
+             ->perform(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                    $accu[] = $obj->name();
+                })
+             ->run();
+        $this->assertEquals(array("dir_2_1"), $accu);
+
+    }
+
 }
