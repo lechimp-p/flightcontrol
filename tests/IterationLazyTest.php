51437d17c0269806994ea30db2a1ef5d2e9f2591
Richard Klees <richard.klees@rwth-aachen.de>
2015-08-02 20:05:13 +0200

    tests for lazy iteration and recursion

---
+++ tests/IterationLazyTest.php
@@ -0,0 +1,168 @@
+<?php
+/******************************************************************************
+ * An iterator interface over the Leagues flysystem.
+ * Copyright (c) 2014, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the along with the code.
+ */
+
+class IterationTest extends _TestCaseBase {
+    public setUp() {
+        parent::setUp();
+        $this->flightcontrol = new \Lechimp\Flightcontrol\Flightcontrol($this->flysystem, false);
+    }
+
+    public function test_correctContents() {
+        $root = $this->flightcontrol->directory("/root");
+        $accu = array();
+        $root->iterateOn()
+             ->with(function (\Lechimp\Flightcontrol\FSObject $obj) use(&$accu) {
+                $accu[] = $obj->name();
+             });
+        $this->assertCount(2, $accu);
+        $this->assertContains("dir_1", $accu);
+        $this->assertContains("dir_2", $accu);
+    }
+
+    public function test_correctContents2() {
+        $dir_2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir_2->iterateOn()
+              ->with(function (\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                $accu[] = $obj->name();
+              });
+        $this->assertCount(2, $accu);
+        $this->assertContains("dir_2_1", $accu);
+        $this->assertContains("file_2_1", $accu);
+    }
+
+    public function test_correctContents3() {
+        $root = $this->flightcontrol->directory("/root");
+        $accu = array();
+        $root
+            ->iterateOn()
+                ->iterateOn()
+                ->with(function (\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                    $accu[] = $obj->name();
+                })
+            ->run();
+        $this->assertCount(4, $accu);
+        $this->assertContains("file_1_1", $accu);
+        $this->assertContains("file_1_2", $accu);
+        $this->assertContains("dir_2_1", $accu);
+        $this->assertContains("file_2_1", $accu);
+    }
+
+    public function test_layeredIteration() {
+        $root = $this->flightcontrol->directory("/root");
+        $accu1 = array();
+        $accu2 = array();
+        $accu3 = array();
+        $in1 = array(false);
+        $in2 = array(false);
+        $in3 = array(false);
+        $root
+            ->iterateOn()
+                ->iterateOn()
+                    ->iterateOn()
+                    ->with(function($obj) use (&$accu3, &$in3) {
+                        $in3[0] = true;
+                        $accu3[] = $obj->name();
+                    })
+                ->with(function($obj) use (&$accu2, &$in2) {
+                    $in2[0] = true;
+                    $accu2[] = $obj->name();
+                })
+            ->with(function($obj) use (&$accu1, &$in1) {
+                $in1[0] = true;
+                $accu1[] = $obj->name();
+            });    
+        $this->assertTrue($in1[0]);
+        $this->assertTrue($in2[0]);
+        $this->assertTrue($in3[0]);
+        $this->assertEquals(array("dir_1", "dir_2"), $accu1);
+        $this->assertEquals(array("file_1_1", "file_1_2", "dir_2_1", "file_2_1"), $accu2);
+        $this->assertEquals(array("file_2_1_1", "file_2_1_2"), $accu3);
+    }
+
+    public function test_filteredLayeredIteration() {
+        $root = $this->flightcontrol->directory("/root");
+        $accu1 = array();
+        $accu2 = array();
+        $accu3 = array();
+        $root
+            ->iterateOn()
+            ->filter(function($obj) {
+                return substr($obj->name(), -1) == "1";
+            })
+                ->iterateOn()
+                ->filter(function($obj) {
+                    return substr($obj->name(), -1) == "1";
+                })
+                    ->iterateOn()
+                    ->filter(function($obj) {
+                        return substr($obj->name(), -1) == "1";
+                    })
+                    ->with(function($obj) use (&$accu3) {
+                        $accu3[] = $obj->name();
+                    })
+                ->with(function($obj) use (&$accu2) {
+                    $accu2[] = $obj->name();
+                })
+            ->with(function($obj) use (&$accu1) {
+                $accu1[] = $obj->name();
+            });    
+        $this->assertEquals(array("dir_1"), $accu1);
+        $this->assertEquals(array("file_1_1"), $accu2);
+        $this->assertEquals(array(), $accu3);
+    }
+
+    public function test_filterWorks() {
+        $root = $this->flightcontrol->directory("/root");
+        $accu = array();
+        $root
+            ->iterateOn()
+            ->filter(function(\Lechimp\Flightcontrol\FSObject $obj) {
+                return $obj->name() == "dir_1";
+            })
+            ->with(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                $accu[] = $obj->name();
+            });
+        $this->assertEquals(array("dir_1"), $accu);
+    } 
+
+    public function test_filterDirectories() {
+        $dir_2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir_2->iterateOn()
+              ->directoriesOnly()
+              ->with(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                $accu[] = $obj->name();
+              });
+        $this->assertEquals(array("dir_2_1"), $accu);
+    }
+
+    public function test_filterFiles() {
+        $dir_2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir_2->iterateOn()
+              ->filesOnly()
+              ->with(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                $accu[] = $obj->name();
+              });
+        $this->assertEquals(array("file_2_1"), $accu);
+    }
+
+    public function test_filterNamed() {
+        $dir2 = $this->flightcontrol->directory("/root/dir_2");
+        $accu = array();
+        $dir2->iterateOn()
+             ->named("dir.*")
+             ->with(function(\Lechimp\Flightcontrol\FSObject $obj) use (&$accu) {
+                $accu[] = $obj->name();
+             });
+        $this->assertEquals(array("dir_2_1"), $accu);
+    }
+
+}
---
+++ tests/RecursionLazyTest.php
@@ -0,0 +1,200 @@
+<?php
+/******************************************************************************
+ * An iterator interface over the Leagues flysystem.
+ * Copyright (c) 2014, 2015 Richard Klees <richard.klees@rwth-aachen.de>
+ *
+ * This software is licensed under The MIT License. You should have received 
+ * a copy of the along with the code.
+ */
+
+class RecursionTest extends _TestCaseBase {
+    public setUp() {
+        parent::setUp();
+        $this->flightcontrol = new \Lechimp\Flightcontrol\Flightcontrol($this->flysystem, false);
+    }
+
+    public function test_allFiles() {
+        $root = $this->flightcontrol->directory("/root");
+        $all_files = array_map( function($obj) { return $obj->name();}
+                              , $root->recurseOn()->allFiles()
+                              );
+        $this->assertCount(5, $all_files);
+        $this->assertContains("file_1_1", $all_files);
+        $this->assertContains("file_1_2", $all_files);
+        $this->assertContains("file_2_1_1", $all_files);
+        $this->assertContains("file_2_1_2", $all_files);
+        $this->assertContains("file_2_1", $all_files);
+    }
+
+    public function test_filteredFiles() {
+        $root = $this->flightcontrol->directory("/root");
+        $all_files = $root
+            ->recurseOn()
+            ->filter(function(\Lechimp\Flightcontrol\FSObject $obj) {
+                return substr($obj->name(), -1) != "1"
+                    || !$obj->isFile()
+                    ;
+            })
+            ->allFiles();
+        $all_files = array_map( function($obj) { return $obj->name();}
+                              , $all_files 
+                              );
+        $this->assertCount(2, $all_files);
+        $this->assertContains("file_1_2", $all_files);
+        $this->assertContains("file_2_1_2", $all_files);
+    }
+
+    public function test_foldFiles() {
+        $root = $this->flightcontrol->directory("/root");
+        $result = $root
+            ->foldFiles(array(), function($accu, \Lechimp\Flightcontrol\File $file) {
+                $accu[] = $file->name(); 
+                return $accu;
+            });
+
+        $this->assertCount(5, $result);
+        $this->assertContains("file_1_1", $result);
+        $this->assertContains("file_1_2", $result);
+        $this->assertContains("file_2_1_1", $result);
+        $this->assertContains("file_2_1_2", $result);
+        $this->assertContains("file_2_1", $result);
+    }
+
+    public function test_filteredFold() {
+        $root = $this->flightcontrol->directory("/root");
+        $result = $root
+            ->recurseOn()
+            ->filter(function(\Lechimp\Flightcontrol\FSObject $obj) {
+                return substr($obj->name(), -1) != "1"
+                    || !$obj->isFile()
+                    ;
+            })
+            ->foldFiles(array(), function($accu, \Lechimp\Flightcontrol\File $file) {
+                $accu[] = $file->name(); 
+                return $accu;
+            });
+
+        $this->assertCount(2, $result);
+        $this->assertContains("file_1_2", $result);
+        $this->assertContains("file_2_1_2", $result);
+    }
+
+    public function test_nameFilteredFold() {
+        $root = $this->flightcontrol->directory("/root");
+        $result = $root
+            ->recurseOn()
+            ->named(".*_1")
+            ->foldFiles(array(), function($accu, \Lechimp\Flightcontrol\File $file) {
+                $accu[] = $file->name(); 
+                return $accu;
+            });
+
+        $this->assertCount(1, $result);
+        $this->assertContains("file_1_1", $result);
+    }
+
+/*    public function test_iteratorHasRecursor() {
+        $root = $this->flightcontrol->directory("/root");
+        $recursor = $root->withContents()->foldFiles();
+        $this->assertInstanceOf("\Lechimp\Flightcontrol\Recursor", $recursor);
+    }
+
+    public function test_filteredIsRecursor() {
+        $root = $this->flightcontrol->directory("/root");
+        $recursor = $root->foldFiles()->filter(function($a) {});
+        $this->assertInstanceOf("\Lechimp\Flightcontrol\Recursor", $recursor);
+    }*/
+
+    public function test_cata1() {
+        $root = $this->flightcontrol->directory("/root");
+        $result = $root
+            ->cata(function(\Lechimp\Flightcontrol\FSObject $obj) {
+                if ($obj->isFile()) {
+                    return array( $obj->name() => $obj->name() );
+                }
+                $merged = call_user_func_array("array_merge", $obj->fcontents());
+                return array( $obj->name() => $merged);
+            });
+        
+        $expected = array
+            ( "root" => array
+                ( "dir_1"   => array
+                    ( "file_1_1" => "file_1_1"
+                    , "file_1_2" => "file_1_2"
+                    )
+                , "dir_2" => array
+                    ( "dir_2_1" => array
+                        ( "file_2_1_1" => "file_2_1_1"
+                        , "file_2_1_2" => "file_2_1_2"
+                        )
+                    , "file_2_1" => "file_2_1"
+                    )
+                )
+            );
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_generalRecursion() {
+        $root = $this->flightcontrol->directory("/root");
+        $result = $root
+            ->recurseOn()
+            ->with(function(\Lechimp\Flightcontrol\FSObject $obj) {
+                if ($obj->isFile()) {
+                    return array($obj->name() => $obj->name());
+                }
+                $merged = call_user_func_array("array_merge", $obj->fcontents());
+                return array( $obj->name() => $merged);
+            });
+        
+        $expected = array
+            ( "root" => array
+                ( "dir_1"   => array
+                    ( "file_1_1" => "file_1_1"
+                    , "file_1_2" => "file_1_2"
+                    )
+                , "dir_2" => array
+                    ( "dir_2_1" => array
+                        ( "file_2_1_1" => "file_2_1_1"
+                        , "file_2_1_2" => "file_2_1_2"
+                        )
+                    , "file_2_1" => "file_2_1"
+                    )
+                )
+            );
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function test_filteredRecursion() {
+        $root = $this->flightcontrol->directory("/root");
+        $result = $root
+            ->recurseOn()
+            ->named("dir.*") 
+            ->with(function (\Lechimp\Flightcontrol\FSObject $obj) {
+                if ($obj->isFile()) {
+                    $this->assertTrue(false);
+                }
+
+                $fcontents = $obj->fcontents();
+                if (empty($fcontents)) {
+                    $merged = $fcontents;
+                }
+                else {
+                    $merged = call_user_func_array("array_merge", $fcontents);
+                }
+                return array( $obj->name() => $merged);
+            });
+        
+        $expected = array
+            ( "root" => array
+                ( "dir_1"   => array()
+                , "dir_2" => array
+                    ( "dir_2_1" => array()
+                    )
+                )
+            );
+
+        $this->assertEquals($expected, $result);
+    }
+}
